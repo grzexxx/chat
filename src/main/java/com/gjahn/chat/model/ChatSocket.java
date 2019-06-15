0e9f5a15bf38e101c8864b5207c29e5e6bfaac0b
GrzexXx <gjahn100@gmail.com>
2019-06-15 12:55:19 +0200

    added: sessions

--- src/main/java/com/gjahn/chat/model/ChatSocket.java
+++ src/main/java/com/gjahn/chat/model/ChatSocket.java
@@ -9,9 +9,14 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EnableWebSocket
 @Component
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+
+    private List<WebSocketSession> sessions = new ArrayList<>();
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
@@ -22,16 +27,23 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("SB connected.");
+        sessions.add(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("Message: " + message.getPayload());
-        session.sendMessage(message);
+
+        for (WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(message);
+        }
+        //   System.out.println("Message: " + message.getPayload());
+        // session.sendMessage(message);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         System.out.println("SB leave.");
+        sessions.remove(session);
     }
+
 }
