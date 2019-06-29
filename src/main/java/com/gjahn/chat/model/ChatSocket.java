88062584a674d7023e981a09f03f6d8c606f99f4
GrzexXx <gjahn100@gmail.com>
2019-06-29 10:50:37 +0200

    some strange adds

--- src/main/java/com/gjahn/chat/model/ChatSocket.java
+++ src/main/java/com/gjahn/chat/model/ChatSocket.java
@@ -9,8 +9,6 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-
-import javax.websocket.Session;
 import java.time.LocalDateTime;
 
 import java.time.format.DateTimeFormatter;
@@ -21,7 +19,8 @@ import java.util.List;
 @Component
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
 
-    private List<WebSocketSession> sessions = new ArrayList<>();
+    // private List<WebSocketSession> sessions = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH mm");
 
     @Override
@@ -33,55 +32,82 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
+        User user = new User(session);
+        users.add(user);
+
+        user.sendMessage("<server> Welcome on chat, first message became your nickname");
         sendWelcomeMessage(session);
     }
 
-    private void sendWelcomeMessage(WebSocketSession session) throws Exception {
-        for (WebSocketSession session1 : sessions) {
-            if (!(session1.getId().equals(session.getId()))) {
+    private void sendWelcomeMessage(WebSocketSession session) throws Exception, NullPointerException {
+        for (User user : users) {
+            if (!(user.getWebSocketSession().getId().equals(session.getId()))) {
 
+                user.sendMessage("<server>SB connected to the chat");
                 TextMessage textMessage = new TextMessage("<server>SB connected to the chat");
-                session1.sendMessage(textMessage);
+                user.getWebSocketSession().sendMessage(textMessage);
             }
         }
     }
 
 
-
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        User user = findUserBySession(session);
 
-        for (WebSocketSession webSocketSession : sessions) {
 
-            TextMessage textMessage = new TextMessage("( "
-                    + formatter.format(LocalDateTime.now())
-                    + " ): " + message.getPayload()
-            );
 
-            webSocketSession.sendMessage(textMessage);
+        if (user.getNickname() == null) {
+            user.setNickname(message.getPayload());
+            user.sendMessage("<server> Now you have nickname");
+            return;
         }
-        //   System.out.println("Message: " + message.getPayload());
-        // session.sendMessage(message);
+
+        for (User user1 : users) {
+            String strnigMessage = user.getNickname() + " ("
+                    + formatter.format(LocalDateTime.now())
+                    + "): " + message.getPayload();
+            user.sendMessage(strnigMessage);
+        }
     }
 
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-       // System.out.println("<server>SB leave.");
         sendDisconnectMessage(session);
-        sessions.remove(session);
+        users.remove(findUserBySession(session));
     }
 
     private void sendDisconnectMessage(WebSocketSession session) throws Exception {
 
-        for (WebSocketSession session1 : sessions) {
-            if (!(session1.getId().equals(session.getId()))) {
+        for (User user : users) {
+            if (!(user.getWebSocketSession().getId().equals(session.getId()))) {
 
                 TextMessage textMessage = new TextMessage("<server>SB leave our chat");
-                session1.sendMessage(textMessage);
+                user.getWebSocketSession().sendMessage(textMessage);
             }
         }
+    }
+
+    private User findUserBySession(WebSocketSession webSocketSession) {
+        for (User user : users) {
+            if (user.getWebSocketSession().getId().equals(webSocketSession.getId())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private boolean userHasNickname(WebSocketSession webSocketSession) {
+
+        for (User user : users) {
+            if (user.getNickname().equals("")) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+
     }
 
 }
--- src/main/java/com/gjahn/chat/model/User.java
+++ src/main/java/com/gjahn/chat/model/User.java
@@ -1,5 +1,41 @@
 package com.gjahn.chat.model;
 
+import lombok.Data;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Date;
+
+@Data
 public class User {
-    private static String nickname;
+
+    private String nickname;
+    private WebSocketSession webSocketSession;
+    private String lastMessTime;
+
+
+    public User (WebSocketSession webSocketSession){
+        this.webSocketSession = webSocketSession;
+    }
+
+    public void sendMessage (String message) throws IOException{
+        webSocketSession.sendMessage(new TextMessage(message));
+    }
+
+  /*  public boolean timeCheck (String timeToCheck) {
+
+
+        Date data1 = format.parse (timeToCheck);
+       Date date2 = format.parse (lastMessTime);
+
+        long difference = date2.getTime() - data1.getTime();
+
+
+        if (diference<1000 ){
+            return true;
+        }
+        return false;
+    }*/
 }
